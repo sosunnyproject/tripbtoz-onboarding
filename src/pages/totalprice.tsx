561403ad31b3ad4f63056b37517c7bd063f818f8
sosunnyproject <parksosun1103@gmail.com>
2020-03-05 16:40:30 +0900

    calendarChange,daysChange

--- src/pages/totalprice.tsx
+++ src/pages/totalprice.tsx
@@ -13,6 +13,7 @@ interface Props {
 }
 
 interface State {
+    selectedDate: any,
     startDate: string;
     endDate: string;
     duration: number;
@@ -22,15 +23,56 @@ class TotalPrice extends React.Component<Props, State> {
   state:State = {
     startDate: '',
     endDate: '',
-    duration: 1
+    duration: 1,
+    selectedDate: null
   };
+
+  calendarChange = (e:any, days: number): void => {
+    console.log("calendarChange called");
+    // console.log(e);
+    if (e == null) {
+      return;
+    } else {
+      let startDate = moment(e).format(dateFormat);  // string YYYY-MM-DD
+      this.setState({ startDate: startDate });
+
+
+
+      let newDate = e.clone();
+      newDate = newDate.add(this.state.duration - 1, 'days').calendar(); // type: string
+      newDate = moment(moment(newDate).toDate()).format('YYYY-MM-DD');  // change format
+      this.setState(() => ({ endDate: newDate }));
+
+      this.dateCalculate(newDate, this.state.duration);
+
+    }
+  }
+
+  daysChange = (value: any):void => {
+    console.log("daysChange called");
+    // this.setState({duration: value});
+    this.setState({duration: value});
+    // console.log("startDate", this.state.startDate);
+
+    let current = moment(this.state.startDate);  // string to moment 
+    let newDate = current.add(value - 1, 'days').calendar(); // type: string
+    newDate = moment(moment(newDate).toDate()).format('YYYY-MM-DD');  // change format
+    this.setState(() => ({ endDate: newDate }));
+    
+    this.dateCalculate(this.state.startDate, value);
+  }
+
+  dateCalculate = (start:any, days:any):void => {
+    console.log("startDate input: " , start);
+    console.log("duration input", days);
+  }
 
   render() {
     return (
       <>
         <div className={styles.normal}>
           <span style={{"margin": "5px"}}>기준일</span>
-          <DatePicker onChange={this.calendarChange} />
+          <DatePicker onChange={(e) => this.calendarChange(e, this.state.duration)} />
           <InputNumber min={1} defaultValue={1} onChange={this.daysChange} />
           <div>
             {this.state.startDate} ~ {this.state.endDate}
@@ -40,27 +82,6 @@ class TotalPrice extends React.Component<Props, State> {
     );
   }
 
-  calendarChange = (date:any, dateString: string): void => {
-    if (date == null) {
-      return;
-    } else {
-      let newDate = date.clone();
-      let daycount = this.state.duration;
-      console.log(daycount);
-      newDate = newDate.add(daycount, 'days').calendar();   
-      newDate = moment(newDate, 'MM/DD/YYYY').toDate(); // js Date object
-      let endDate = moment(newDate).format('YYYY-MM-DD');
-      console.log(`start: ${dateString}`);
-      console.log(`end: ${endDate}`);
-      console.log(`duration: ${this.state.duration}`);
-      this.setState({ startDate: dateString })
-      this.setState({ endDate: endDate })
-    }
-  }
-
-  daysChange = (value:any):void => {
-    this.setState({duration: value-1});
-  }
 
 }
 
